cfd6fb7c216e341f09e502250abae1d2efbd3cf6
Zsl <njuptsniper@gmail.com>
2016-04-03 21:55:00 +0800

    添加定位功能。

--- app/src/main/java/com/njupt/sniper/smartparking/MapActivity.java
+++ app/src/main/java/com/njupt/sniper/smartparking/MapActivity.java
@@ -2,20 +2,126 @@ package com.njupt.sniper.smartparking;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationConfiguration;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
 
 /**
  * Created by Administrator on 2016/4/3.
  */
 public class MapActivity extends AppCompatActivity {
     private MapView mMapView;
+    private BaiduMap mBaiduMap;
+
+    public LocationClient mLocationClient = null;
+
+    private TextView tv_location_data;
+    private Button btn_request_location;
+
+
+
+    public BDLocationListener myListener = new MyLocationListener() {
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            super.onReceiveLocation(location);
+            initMyLocation(location);
+        }
+    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_map);
         mMapView = (MapView) findViewById(R.id.bmapView);
+        mBaiduMap = mMapView.getMap();
+
+        tv_location_data= (TextView) findViewById(R.id.tv_location_data);
+        btn_request_location= (Button) findViewById(R.id.btn_request_location);
+        btn_request_location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mLocationClient.requestLocation();// 发起定位请求
+            }
+        });
+
+        mLocationClient = new LocationClient(getApplicationContext());     //声明LocationClient类
+        mLocationClient.registerLocationListener(myListener);    //注册监听函数
+
+        initLocation();
+
+        mLocationClient.start();
+     //   mLocationClient.requestLocation();// 发起定位请求
+
+    }
+
+
+    //初始化我的当前位置
+    public void initMyLocation(BDLocation location) {
+        MyLocationData.Builder builder = new MyLocationData.Builder();
+        builder.latitude(location.getLatitude());//设置纬度
+        builder.longitude(location.getLongitude());//设置经度
+        builder.accuracy(location.getRadius());//设置精度（半径）
+        builder.direction(location.getDirection());//设置方向
+        builder.speed(location.getSpeed());//设置速度
+        MyLocationData locationData = builder.build();
+
+        mBaiduMap.setMyLocationData(locationData);
+
+        LatLng latlng = new LatLng(location.getLatitude(), location.getLongitude());
+
+        //----------添加一个标注覆盖物在当前位置--------------
+        //构建Marker图标
+        BitmapDescriptor mCurrentMarker = BitmapDescriptorFactory
+                .fromResource(R.drawable.icon_geo);
+        //构建MarkerOption，用于在地图上添加Marker
+        //OverlayOptions option = new MarkerOptions()
+        // .position(latlng)
+        // .icon(bitmap);
+        //在地图上添加Marker，并显示
+        //baiduMap.addOverlay(option);
+
+        //配置定位图层显示方式(显示模式（普通，罗盘，跟随）)
+        // mode - 定位图层显示方式, 默认为 LocationMode.NORMAL 普通态
+        //enableDirection - 是否允许显示方向信息
+        //customMarker - 设置用户自定义定位图标，可以为 null
+        mBaiduMap.setMyLocationConfigeration(new MyLocationConfiguration(com.baidu.mapapi.map.MyLocationConfiguration.LocationMode.COMPASS, true, mCurrentMarker));
+
+
+        //设置我的位置为地图的中心点
+        mBaiduMap.animateMapStatus(MapStatusUpdateFactory.newLatLng(latlng));
+    }
+
+
+    private void initLocation() {
+        LocationClientOption option = new LocationClientOption();
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy
+        );//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
+        option.setCoorType("bd09ll");//可选，默认gcj02，设置返回的定位结果坐标系
+        int span = 1000;
+        option.setScanSpan(span);//可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
+        option.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
+        option.setOpenGps(true);//可选，默认false,设置是否使用gps
+        option.setLocationNotify(true);//可选，默认false，设置是否当gps有效时按照1S1次频率输出GPS结果
+        option.setIsNeedLocationDescribe(true);//可选，默认false，设置是否需要位置语义化结果，可以在BDLocation.getLocationDescribe里得到，结果类似于“在北京天安门附近”
+        option.setIsNeedLocationPoiList(true);//可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
+        option.setIgnoreKillProcess(false);//可选，默认true，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认不杀死
+        option.SetIgnoreCacheException(false);//可选，默认false，设置是否收集CRASH信息，默认收集
+        option.setEnableSimulateGps(false);//可选，默认false，设置是否需要过滤gps仿真结果，默认需要
+        mLocationClient.setLocOption(option);
     }
 
     @Override
@@ -24,16 +130,51 @@ public class MapActivity extends AppCompatActivity {
         //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
         mMapView.onDestroy();
     }
+
     @Override
     protected void onResume() {
         super.onResume();
         //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
         mMapView.onResume();
     }
+
     @Override
     protected void onPause() {
         super.onPause();
         //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
         mMapView.onPause();
+    }
+
+    class MyLocationListener implements BDLocationListener {
+
+
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            if (location == null)
+                return;
+            StringBuffer sb = new StringBuffer(256);
+            sb.append("time : ");
+            sb.append(location.getTime());
+            sb.append("\nerror code : ");
+            sb.append(location.getLocType());
+            sb.append("\nlatitude : ");
+            sb.append(location.getLatitude());
+            sb.append("\nlontitude : ");
+            sb.append(location.getLongitude());
+            sb.append("\nradius : ");
+            sb.append(location.getRadius());
+            if (location.getLocType() == BDLocation.TypeGpsLocation) {
+                sb.append("\nspeed : ");
+                sb.append(location.getSpeed());
+                sb.append("\nsatellite : ");
+                sb.append(location.getSatelliteNumber());
+            } else if (location.getLocType() == BDLocation.TypeNetWorkLocation) {
+                sb.append("\naddr : ");
+                sb.append(location.getAddrStr());
+            }
+            System.out.println(sb);
+            tv_location_data.setText(sb);
+            initMyLocation(location);
+        }
     }
 }
